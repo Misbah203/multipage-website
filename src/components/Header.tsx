2b6d84ab81a7550849d15cb2b7b9eeaeb38c663f
Misbah203 <misbaharand203@gmail.com>
2025-01-18 00:48:24 +0500

    multipage-website

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,16 @@ import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () =>{
     return(
-        <header className='header'>
-        <h1 className='logo'>Donuts World</h1>
-      <nav>
-      <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/donuts">Donuts</Link>
-        <Link href="/contact" >ContactUs</Link>
-        <FaShoppingCart size={20} />
-      </nav>
-       </header>
+      <header className='header'>
+      <h1 className='logo'>Donuts World</h1>
+    <nav>
+    <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/donuts">Donuts</Link>
+      <Link href="/contact" >ContactUs</Link>
+      <FaShoppingCart size={20} />
+    </nav>
+     </header>
     )
 
 }
